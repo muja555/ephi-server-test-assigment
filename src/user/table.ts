16d6659ea9340fa885ce94bb0f3e2da4d735cda8
Muja Dameri <mujahedD5@gmail.com>
2023-03-08 11:49:24 +0200

    add more users

--- src/user/table.ts
+++ src/user/table.ts
@@ -21,7 +21,37 @@ const UsersTable = new Map<number, IUser>(
             password: '1',
             ephi_id: null,
         }
-        ]
+        ],
+        [4, {
+            user_id: 4,
+            username: '4',
+            password: '4',
+            ephi_id: null,
+        }],
+        [5, {
+            user_id: 5,
+            username: '2',
+            password: '2',
+            ephi_id: null,
+        }],
+        [6, {
+            user_id: 6,
+            username: '3',
+            password: '3',
+            ephi_id: null,
+        }],
+        [7, {
+            user_id: 7,
+            username: '4',
+            password: '4',
+            ephi_id: null,
+        }],
+        [8, {
+            user_id: 7,
+            username: '5',
+            password: '5',
+            ephi_id: null,
+        }],
     ]);
 
 export default UsersTable;

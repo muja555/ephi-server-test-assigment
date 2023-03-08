b80316ef5a4516eda3735912f277b532ce062f07
Muja Dameri <mujahedD5@gmail.com>
2023-03-08 10:25:06 +0200

    add users

--- src/user/table.ts
+++ src/user/table.ts
@@ -3,11 +3,24 @@ import {IUser} from "./types.js";
 const UsersTable = new Map<number, IUser>(
     [
         [1, {
-                user_id: 1,
-                username: 'test',
-                password: '123456',
-                ephi_id: null,
-            }
+            user_id: 1,
+            username: 'test',
+            password: '123456',
+            ephi_id: null,
+        }
+        ],
+        [2, {
+            user_id: 2,
+            username: '1',
+            password: '1',
+            ephi_id: null,
+        }],
+        [3, {
+            user_id: 3,
+            username: 't',
+            password: '1',
+            ephi_id: null,
+        }
         ]
     ]);
 
